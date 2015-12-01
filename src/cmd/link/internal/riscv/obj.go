2ae15a12180b53c64fa2e7b6357abb7771bcd801
Benjamin Barenblat <bbaren@google.com>
2015-12-01 15:58:36 -0500

    cmd/link/internal/riscv/obj: Implement archinit
    When the linker starts up, set link mode correctly and prepare to build
    an ELF.

--- src/cmd/link/internal/riscv/obj.go
+++ src/cmd/link/internal/riscv/obj.go
@@ -1,7 +1,9 @@
 package riscv
 
 import (
+	"cmd/internal/obj"
 	"cmd/link/internal/ld"
+	"fmt"
 	"log"
 )
 
@@ -48,6 +50,37 @@ func linkarchinit() {
 }
 
 func archinit() {
-	// TODO(bbaren): Implement
 	log.Printf("archinit")
+
+	// TODO(bbaren): Support external linking
+	if ld.Linkmode == ld.LinkExternal {
+		log.Fatalf("-linkmode=external is unsupported on RISC-V")
+	} else {
+		ld.Linkmode = ld.LinkInternal
+	}
+
+	// TODO(bbaren): This is mostly cargo-culted off the arm and amd64
+	// backends.  Figure out what this does and document it.
+	switch ld.HEADTYPE {
+	default:
+		ld.Exitf("unknown -H option: %v", ld.HEADTYPE)
+
+	case obj.Hlinux:
+		ld.Elfinit()
+		ld.HEADR = ld.ELFRESERVE
+		if ld.INITTEXT == -1 {
+			ld.INITTEXT = (1 << 22) + int64(ld.HEADR)
+		}
+		if ld.INITDAT == -1 {
+			ld.INITDAT = 0
+		}
+		if ld.INITRND == -1 {
+			ld.INITRND = 4096
+		}
+	}
+
+	if ld.INITDAT != 0 && ld.INITRND != 0 {
+		fmt.Printf("warning: -D0x%x is ignored because of -R0x%x\n",
+			uint64(ld.INITDAT), uint32(ld.INITRND))
+	}
 }
