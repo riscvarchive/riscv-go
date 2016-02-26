afdef650b144df866d6f1727225025996da94ab1
Benjamin Barenblat <bbaren@google.com>
2016-02-26 12:08:06 -0500

    Add end-to-end test for RISC-V assembler
    You can run the end-to-end test with
    
      go test -test.run TestRISCVEncoder cmd/asm/internal/asm
    
    from the src directory.
    
    Change-Id: Id17f456621d23d02a609b4266132b8b11fdfb077

---
+++ src/cmd/asm/internal/asm/testdata/riscvenc.s
@@ -0,0 +1,32 @@
+TEXT asmtest(SB),7,$0
+start:
+	ADD	T1, T0, T2			// b3836200
+	ADD	T0, T1				// 33035300
+	ADD	$2047, T0, T1			// 1383f27f
+	ADD	$-2048, T0, T1			// 13830280
+	ADD	$2047, T0			// 9382f27f
+	ADD	$-2048, T0			// 93820280
+
+	SUB	T1, T0, T2			// b3836240
+	SUB	T0, T1				// 33035340
+
+	SLL	T1, T0, T2			// b3936200
+	SLL	T0, T1				// 33135300
+	SLL	$1, T0, T1			// 13931200
+	SLL	$1, T0				// 93921200
+	SRL	T1, T0, T2			// b3d36200
+	SRL	T0, T1				// 33535300
+	SRL	$1, T0, T1			// 13d31200
+	SRL	$1, T0				// 93d21200
+	SRA	T1, T0, T2			// b3d36240
+	SRA	T0, T1				// 33535340
+	SRA	$1, T0, T1			// 13d31200
+	SRA	$1, T0				// 93d21200
+
+	// This jump can get printed as JMP 2 because it goes to the second
+	// instruction in the function.  (The first instruction is an invisible
+	// stack pointer adjustment.)
+	JMP	start		// JMP	2	// 6ff01ffb
+
+	// TODO(bbaren): Fix RDCYCLE in the assembler and uncomment this line.
+	// RDCYCLE	T0				// f32200c0
