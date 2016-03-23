2b4e1e64b6d88b9b7ba4001cc727f303cfcae2e7
Michael Pratt <michael@pratt.im>
2016-03-22 23:54:26 -0700

    runtime, compile: compile a bit more of the runtime for RISCV
    At this point, we actually need to implement some more of the compiler.
    
    Change-Id: I7bbecee73d09f2f61929a1a423f52f1f71ef24ff

---
+++ src/cmd/compile/internal/ssa/rewriteRISCV.go
@@ -0,0 +1,11 @@
+// TODO(prattmic): autogenerate this
+
+package ssa
+
+func rewriteValueRISCV(v *Value, config *Config) bool {
+	return false
+}
+
+func rewriteBlockRISCV(b *Block) bool {
+	return false
+}
