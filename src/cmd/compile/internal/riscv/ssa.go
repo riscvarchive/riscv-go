d2b91250c9065ba06d33b53c1604888cc134b6e9
Michael Pratt <michael@pratt.im>
2016-03-29 22:27:15 -0700

    cmd/compile/internal: initial RISCV SSA lowering operations
    Add initial support for lowering SSA IR to RISCV Progs.
    
    Only the ADD op is added for now, but it is used successfully in the
    compiler. Before this change, the compiler complains about failing to
    lower NilCheck. With this change it moves on to the next operation.
    
    Note that the opGen.go and rewriteRISCV.go changes are autogenerated.
    
    Change-Id: I3ae9da011852373966784ba914144c4aacef39d1

--- src/cmd/compile/internal/riscv/ssa.go
+++ src/cmd/compile/internal/riscv/ssa.go
@@ -9,6 +9,7 @@ import (
 
 	"cmd/compile/internal/gc"
 	"cmd/compile/internal/ssa"
+	"cmd/internal/obj"
 )
 
 // ssaRegToReg maps ssa register numbers to obj register numbers.
@@ -20,7 +21,23 @@ func ssaMarkMoves(s *gc.SSAGenState, b *ssa.Block) {
 }
 
 func ssaGenValue(s *gc.SSAGenState, v *ssa.Value) {
-	log.Printf("ssaGenValues")
+	log.Printf("ssaGenValue")
+	s.SetLineno(v.Line)
+
+	switch v.Op {
+	case ssa.OpRISCVADD:
+		r := gc.SSARegNum(v)
+		r1 := gc.SSARegNum(v.Args[0])
+		r2 := gc.SSARegNum(v.Args[1])
+		p := gc.Prog(v.Op.Asm())
+		p.From.Type = obj.TYPE_REG
+		p.From.Reg = r1
+		p.From3 = &obj.Addr{}
+		p.From3.Type = obj.TYPE_REG
+		p.From3.Reg = r2
+		p.To.Type = obj.TYPE_REG
+		p.To.Reg = r
+	}
 }
 
 func ssaGenBlock(s *gc.SSAGenState, b, next *ssa.Block) {
