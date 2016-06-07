58e770000eac4a209948af5cd19f778b9443ca62
Josh Bleecher Snyder <josharian@gmail.com>
2016-06-07 16:11:14 -0700

    riscv_hello: print H to stdout
    This is a baby step towards a useful test rig.
    It prints the letter H to stdout
    and sets the rc to whether it succeeded.
    Next step is to support function calls,
    and set up a better printing facility.

--- src/cmd/asm/internal/asm/testdata/riscv_hello.s
+++ src/cmd/asm/internal/asm/testdata/riscv_hello.s
@@ -1,9 +1,21 @@
 #define SYS_EXIT_GROUP	94
+#define SYS_WRITE	64
 
 // _rt0_riscv_linux is the entry point.
-TEXT _rt0_riscv_linux(SB),0,$0
-
+TEXT _rt0_riscv_linux(SB),0,$8
+	// Write "H" to stdout...not quite to hello world, yet
+	MOV	$72, T0 // 'H'
+	MOVW	T0, 0(SP)
+	MOV	SP, A1 // ptr to data
+	MOV	$1, A0 // fd 1 for stdout
+	MOV	$1, A2 // len("H") == 1
+	MOV	$SYS_WRITE, A7
+	SCALL
+	// A0 is return value from syscall, convert to 0/1 for use as exit code and
+	// put back in A0 for next syscall.
+	// Note that, as the spec observes, SLTIU rd, rs1, 1 == SEQZ rd, rs1
+	// TODO: Add SEQZ support directly
+	SLTIU	A0, $1, A0 
 	MOV	$SYS_EXIT_GROUP, A7
-	MOV	$42, A0	// exit code
 	SCALL
 	RET
