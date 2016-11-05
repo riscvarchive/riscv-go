8a856ab2b53c9ed01a96607144d592ae5d46ac7d
Amol Bhave <ammubhave@gmail.com>
2016-11-05 14:54:02 -0400

    cmd/compile/internal: Fix riscv ginsnop() to generate ADDI $0, ZERO
    ginsnop() used to generate ADD ZERO, ZERO. The actual NOP defined
    is an ADDI instruction. This commit fixes the NOP generation, and
    generates a ADDI $0, ZERO, ZERO
    
    Change-Id: I0b5cc6be289277d1edcc0014dda6be9d3cf5e406

--- src/cmd/compile/internal/riscv/gsubr.go
+++ src/cmd/compile/internal/riscv/gsubr.go
@@ -13,6 +13,8 @@ import (
 func ginsnop() {
 	// Hardware nop is ADD $0, ZERO
 	p := gc.Prog(riscv.AADD)
-	p.To.Type = obj.TYPE_REG
-	p.To.Reg = riscv.REG_ZERO
+	p.From.Type = obj.TYPE_CONST
+	p.From.Offset = 0
+	p.From3 = &obj.Addr{Type: obj.TYPE_REG, Reg: riscv.REG_ZERO}
+	p.To = *p.From3
 }
