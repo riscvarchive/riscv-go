2fbedede14999082c926b9a1f3db08f44a388921
Benjamin Barenblat <bbaren@google.com>
2016-11-09 16:12:24 -0500

    runtime/internal/atomic: Support RISC-V
    Change-Id: Iba1940ac82839e505f3a0e3d187528d9ed6c5081

---
+++ src/runtime/internal/atomic/asm_riscv.s
@@ -0,0 +1,72 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build riscv
+
+#include "textflag.h"
+
+TEXT ·Cas(SB), NOSPLIT, $0-32
+	MOV	addr+0(FP), A0
+	MOVW	old+8(FP), A1
+	MOVW	new+16(FP), A2
+cas:
+	WORD $0x140522af	// lr.w.aq t0,(a0)
+	BNE	T0, A1, fail
+	WORD $0x1cc5252f	// sc.w.aq a0,a2,(a0)
+	// a0 = 0 iff the sc succeeded, a0 = 0. Convert that to a boolean.
+	SLTIU	$1, A0, A0
+	MOV	A0, swapped+24(FP)
+	RET
+fail:
+	MOV	$0, A0
+	MOV	A0, swapped+24(FP)
+	RET
+
+TEXT ·Casp1(SB), NOSPLIT, $0-25
+	MOV	addr+0(FP), A0
+	MOV	old+8(FP), A1
+	MOV	new+16(FP), A2
+cas:
+	WORD $0x140532af	// lr.d.aq t0,(a0)
+	BNE	T0, A1, fail
+	WORD $0x1cc5352f	// sc.d.aq a0,a2,(a0)
+	// a0 = 0 iff the sc succeeded, a0 = 0. Convert that to a boolean.
+	SLTIU	$1, A0, A0
+	MOV	A0, swapped+24(FP)
+	RET
+fail:
+	MOV	$0, A0
+	MOV	A0, swapped+24(FP)
+	RET
+
+TEXT ·Casuintptr(SB),NOSPLIT,$0-32
+	JMP ·Casp1(SB)
+
+TEXT ·StoreUintptr(SB),NOSPLIT,$0-16
+	MOV	addr+0(FP), A0
+	MOV	val+8(FP), A1
+	// Since addr is aligned, this store is atomic automatically.
+	MOV	A1, (A0)
+	RET
+
+TEXT ·Loaduintptr(SB),NOSPLIT,$0-16
+	MOV	addr+0(FP), A0
+	// Since addr is aligned, this load is atomic automatically.
+	MOV	(A0), A0
+	MOV	A0, val+8(FP)
+	RET
+
+TEXT ·Loaduint(SB),NOSPLIT,$0-16
+	JMP ·Loaduintptr(SB)
+
+TEXT ·Loadint64(SB),NOSPLIT,$0-16
+	JMP ·Loaduintptr(SB)
+
+TEXT ·Xaddint64(SB),NOSPLIT,$0-24
+	MOV	addr+0(FP), A0
+	MOV	delta+8(FP), A1
+	WORD $0x04b5352f	// amoadd.d.aq a0,a1,(a0)
+	ADD	A0, A1, A0
+	MOVW	A0, new+16(FP)
+	RET
