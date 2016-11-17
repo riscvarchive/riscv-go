9f007117001c51fec91ee75fe5a69955f05e4c9a
Amol Bhave <ammubhave@gmail.com>
2016-11-17 01:05:22 +0000

    cmd/compile: Fix float32 constant generation
    Fixes #13
    
    math.Float32bits returns a uint32, and then a cast to int64 used to convert
    it into a 64bit immediate which overflowed the signed int32 bounds.
    Casting it into a signed int32 first forces the compiler to sign extend
    the value returned by math.Float32bits which correctly generates the 32bit
    signed immediate for the MOV instruction.
    
    Change-Id: Ia531a26eeb4e344aff490ab73a35b8cf31c9e6dc

--- riscvtest/fmv.go
+++ riscvtest/fmv.go
@@ -2,5 +2,14 @@ package main
 
 func main() {
 	a := float32(5)
-	riscvexit(int(a))
+	if int(a) != 5 {
+		riscvexit(1)
+	}
+
+	b := float32(1e6)
+	if b < -1e7 || b > 1e7 {
+		riscvexit(2)
+	}
+
+	riscvexit(0)
 }
