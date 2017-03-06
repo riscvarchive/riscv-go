d98909413b48701bd584c4af8dc995ca008ab348
Stefan O'Rear <sorear2@gmail.com>
2017-03-06 00:57:12 -0800

    syscall: riscv: add RawSyscall* variants
    Change-Id: Ib143d79e55395455558914526d222328c6212d0a
    Reviewed-on: https://review.gerrithub.io/352809
    Reviewed-by: Michael Pratt <michael@pratt.im>

--- src/syscall/asm_linux_riscv.s
+++ src/syscall/asm_linux_riscv.s
@@ -11,7 +11,7 @@
 // FIXME call entersyscall/exitsyscall once the runtime is working
 // func Syscall(trap int64, a1, a2, a3 int64) (r1, r2, err int64)
 TEXT ·Syscall(SB),NOSPLIT,$0-56
-	//CALL	runtime·entersyscall(SB)
+	CALL	runtime·entersyscall(SB)
 	MOV	a1+8(FP), A0
 	MOV	a2+16(FP), A1
 	MOV	a3+24(FP), A2
@@ -26,7 +26,7 @@ TEXT ·Syscall(SB),NOSPLIT,$0-56
 	MOV	A0, r1+32(FP)	// r1
 	MOV	A1, r2+40(FP)	// r2
 	MOV	ZERO, err+48(FP)	// errno
-	//CALL	runtime·exitsyscall(SB)
+	CALL	runtime·exitsyscall(SB)
 	RET
 err:
 	MOV	$-1, T0
@@ -34,12 +34,12 @@ err:
 	MOV	ZERO, r2+40(FP)	// r2
 	SUB	A0, ZERO, A0
 	MOV	A0, err+48(FP)	// errno
-	//CALL	runtime·exitsyscall(SB)
+	CALL	runtime·exitsyscall(SB)
 	RET
 
 // func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
 TEXT ·Syscall6(SB),NOSPLIT,$0-80
-	//CALL	runtime·entersyscall(SB)
+	CALL	runtime·entersyscall(SB)
 	MOV	a1+8(FP), A0
 	MOV	a2+16(FP), A1
 	MOV	a3+24(FP), A2
@@ -54,7 +54,7 @@ TEXT ·Syscall6(SB),NOSPLIT,$0-80
 	MOV	A0, r1+56(FP)	// r1
 	MOV	A1, r2+64(FP)	// r2
 	MOV	ZERO, err+72(FP)	// errno
-	//CALL	runtime·exitsyscall(SB)
+	CALL	runtime·exitsyscall(SB)
 	RET
 err:
 	MOV	$-1, T0
@@ -62,13 +62,55 @@ err:
 	MOV	ZERO, r2+64(FP)	// r2
 	SUB	A0, ZERO, A0
 	MOV	A0, err+72(FP)	// errno
-	//CALL	runtime·exitsyscall(SB)
+	CALL	runtime·exitsyscall(SB)
 	RET
 
 // func RawSyscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr)
 TEXT ·RawSyscall(SB),NOSPLIT,$0-56
-	WORD $0
+	MOV	a1+8(FP), A0
+	MOV	a2+16(FP), A1
+	MOV	a3+24(FP), A2
+	MOV	$0, A3
+	MOV	$0, A4
+	MOV	$0, A5
+	MOV	$0, A6
+	MOV	trap+0(FP), A7	// syscall entry
+	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
+	MOV	A0, r1+32(FP)	// r1
+	MOV	A1, r2+40(FP)	// r2
+	MOV	ZERO, err+48(FP)	// errno
+	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+32(FP)	// r1
+	MOV	ZERO, r2+40(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+48(FP)	// errno
+	RET
 
 // func RawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
 TEXT ·RawSyscall6(SB),NOSPLIT,$0-80
-	WORD $0
+	MOV	a1+8(FP), A0
+	MOV	a2+16(FP), A1
+	MOV	a3+24(FP), A2
+	MOV	a4+32(FP), A3
+	MOV	a5+40(FP), A4
+	MOV	a6+48(FP), A5
+	MOV	$0, A6
+	MOV	trap+0(FP), A7	// syscall entry
+	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
+	MOV	A0, r1+56(FP)	// r1
+	MOV	A1, r2+64(FP)	// r2
+	MOV	ZERO, err+72(FP)	// errno
+	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+56(FP)	// r1
+	MOV	ZERO, r2+64(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+72(FP)	// errno
+	RET
